ac01da52e5acc45053dd2b67e16808c24ad7323f
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-05-23 23:38:34 +0200

    Update Event.py

--- Event.py
+++ Event.py
@@ -1,4 +1,13 @@
 from pimoroni import Button
 
+button_up = Button(22, invert=False)
+button_down = Button(6, invert=False)
 button_a = Button(7, invert=False)
+button_b = Button(8, invert=False)
 button_c = Button(9, invert=False)
+lux_vref_pwr = Pin(27, Pin.OUT)
+lux = ADC(26)
+vbat_adc = ADC(29)
+vref_adc = ADC(28)
+usb_power = Pin(24, Pin.IN)
+led = Pin(25, Pin.OUT)
