76fd491ecd75c02915d65a3c6ae8716a37d2f49e
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-05-24 23:45:44 +0200

    Update main.py
    First ok

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-#Snowglobe Badge V0.1 Remasterized
-import Display
-import Event
-import setting
+import event, display, bat
 import time
-import jpegdec
-import qrcode
-
-
-WIDTH, HEIGHT = display.get_bounds()
-
-# Some constants we'll use for drawing
-BORDER_SIZE = 4
-PADDING = 10
-COMPANY_HEIGHT = 40
-
-    # Draw QR button label
-    display.set_pen(LIGHTEST)
-    display.text("QR", 240, 215, 160, 2)
-
 
 # draw the badge for the first time
-
-draw_badge()
-show_photo()
+badge_mode = "photo"
+display.draw_badge()
+display.show_photo()
 display.update()
 
 while True:
-	if badge_mode == "photo":
-		draw_badge()
-        show_photo()
+    display.t = time.ticks_ms() / 1000.0
+    display.clear()
+    bat.measure_battery()
+    if badge_mode == "photo":
+        display.draw_badge()
+        display.show_photo()
+        display.hightext()
+        
+
         display.update()
-	    if button_c.is_pressed:
+    
+    if event.button_c.is_pressed:
+        if badge_mode == "photo":
             badge_mode = "qr"
-            show_qr()
+            display.show_qr()
             display.update()
-    time.sleep(1) 
+        else:
+            badge_mode = "photo"
+            display.draw_badge()
+            display.show_photo()
+            display.update()
+        time.sleep(1)
+    if event.button_a.is_pressed:
+        __import__("menu.py")
+    if event.button_b.is_pressed:
+        if badge_mode == "photo":
+            badge_mode = "bat"
+            display.clear()
+            bmode_status_tick()
+        else:
+            badge_mode = "photo"
+            draw_badge()
+            show_photo()
+            display.update()
+        time.sleep(1)
