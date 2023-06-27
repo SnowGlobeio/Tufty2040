892bfeaa8194a4888d2e80dceecf3df924076983
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-06-27 22:56:16 +0200

    Update main.py
    Working V1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import event, display, bat
 import time
+from lux import *
 
 # draw the badge for the first time
 badge_mode = "photo"
@@ -7,17 +8,27 @@ display.draw_badge()
 display.show_photo()
 display.update()
 
+backlight = BACKLIGHT_LOW
 while True:
-    display.t = time.ticks_ms() / 1000.0
-    display.clear()
+    display.t = time.ticks_ms() / 800.0
+    lux_vref_pwr.value(1)
+    (vbat, on_usb, low_battery) = measure_battery()
     bat.measure_battery()
+    if low_battery:
+        backlight = BACKLIGHT_LOW
+    else:
+        (luminance, backlight) = auto_brightness(backlight)
+    lux_vref_pwr.value(0)
+    display.set_backlight(backlight)
     if badge_mode == "photo":
         display.draw_badge()
-        display.show_photo()
         display.hightext()
-        
-
+        display.show_photo()
+        #display.hightext()
         display.update()
+    
+    if badge_mode == "bat":
+        display.bmode_status_tick()
     
     if event.button_c.is_pressed:
         if badge_mode == "photo":
@@ -31,15 +42,16 @@ while True:
             display.update()
         time.sleep(1)
     if event.button_a.is_pressed:
-        __import__("menu.py")
+        if badge_mode != "bat" or "qr":
+          __import__("menu.py")
     if event.button_b.is_pressed:
         if badge_mode == "photo":
             badge_mode = "bat"
             display.clear()
-            bmode_status_tick()
+            display.bmode_status_tick()
         else:
             badge_mode = "photo"
-            draw_badge()
-            show_photo()
+            display.draw_badge()
+            display.show_photo()
             display.update()
         time.sleep(1)
